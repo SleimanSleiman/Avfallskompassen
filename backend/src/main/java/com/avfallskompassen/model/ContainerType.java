86d2290b4daae70323f30e1dabfb873c3f127000
loubro09 <louis.r.brown@hotmail.com>
2025-10-13 11:43:20 +0200

    NSR-147: getters+setters containertype

--- backend/src/main/java/com/avfallskompassen/model/ContainerType.java
+++ backend/src/main/java/com/avfallskompassen/model/ContainerType.java
@@ -55,4 +55,44 @@ public class ContainerType {
     public void setName(String name) {
         this.name = name;
     }
+
+    public double getWidth() {
+        return width;
+    }
+    
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getImageFrontViewUrl() {
+        return imageFrontViewUrl;
+    }
+
+    public void setImageFrontViewUrl(String imageFrontViewUrl) {
+        this.imageFrontViewUrl = imageFrontViewUrl;
+    }
+
+    public String getImageTopViewUrl() {
+        return imageTopViewUrl;
+    }
+
+    public void setImageTopViewUrl(String imageTopViewUrl) {
+        this.imageTopViewUrl = imageTopViewUrl;
+    }
 }
