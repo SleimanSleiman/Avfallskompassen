e217b98f2178215c92234fc07f7e3884ab15f88a
StorckC <119129240+StorckC@users.noreply.github.com>
2025-10-22 22:49:08 +0200

    Update PropertyContainerRepository.java
    Updated ONE name of a method ...

--- backend/src/main/java/com/avfallskompassen/repository/PropertyContainerRepository.java
+++ backend/src/main/java/com/avfallskompassen/repository/PropertyContainerRepository.java
@@ -12,5 +12,5 @@ import java.util.List;
 
 @Repository
 public interface PropertyContainerRepository extends JpaRepository<PropertyContainer, Long> {
-    List<PropertyContainer> findByProperty(Long propertyId);
+    List<PropertyContainer> findByPropertyId(Long propertyId);
 }
