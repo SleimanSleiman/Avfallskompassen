9f1be8edf029156bf95e9d2c4b12d5ca085b8f15
Anton029040 <2002anton02@gmail.com>
2025-10-24 14:17:30 +0200

    NSR-200/bug/Fix small Errors
    Fixade ett problem med statuskod samt ett problem med att det inte gick att uppdatera soprum

--- backend/src/main/java/com/avfallskompassen/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/avfallskompassen/exception/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package com.avfallskompassen.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -24,4 +27,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Something went wrong in the server"));
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
