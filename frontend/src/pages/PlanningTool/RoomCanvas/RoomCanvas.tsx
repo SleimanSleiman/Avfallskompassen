da8424c668be650e2e4f1e93def722bf7beb87f1
loubro09 <louis.r.brown@hotmail.com>
2025-11-07 14:49:01 +0100

    Update RoomCanvas.tsx

--- frontend/src/pages/PlanningTool/RoomCanvas/RoomCanvas.tsx
+++ frontend/src/pages/PlanningTool/RoomCanvas/RoomCanvas.tsx
@@ -86,8 +86,8 @@ export default function RoomCanvas({
         setRoom({
             x: (STAGE_WIDTH - length / SCALE) / 2,
             y: (STAGE_HEIGHT - width / SCALE) / 2,
-            width: length / SCALE,
-            height: width / SCALE,
+            width: width / SCALE,
+            height: length / SCALE,
         });
         setIsRoomPromptOpen(false);
       };
