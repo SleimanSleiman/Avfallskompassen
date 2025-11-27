2e401e0180236886423e312580249b7c591f2e70
loubro09 <louis.r.brown@hotmail.com>
2025-11-27 13:33:28 +0100

    NSR-399: dörrmått syns bara för selected dörr

--- frontend/src/pages/PlanningTool/RoomCanvas/RoomCanvas.tsx
+++ frontend/src/pages/PlanningTool/RoomCanvas/RoomCanvas.tsx
@@ -293,6 +293,7 @@ export default function RoomCanvas({
                             <DoorMeasurementLayer
                                 doors={doors}
                                 room={room}
+                                selectedDoorId={selectedDoorId}
                             />
 
                             {/* Containers layer */}
